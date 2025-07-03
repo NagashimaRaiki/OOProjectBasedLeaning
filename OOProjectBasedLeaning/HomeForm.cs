1c0a6c42514489d116824da5f0d618fb25218c3a
kazu0 <C3309@oic.jp>
2025-07-03 16:10:14 +0900

    HomeFormコード途中

--- OOProjectBasedLeaning/HomeForm.cs
+++ OOProjectBasedLeaning/HomeForm.cs
@@ -5,6 +5,8 @@ namespace OOProjectBasedLeaning
 
     public partial class HomeForm : DragDropForm
     {
+        private Company company = NullCompany.Instance;
+        private Label employeeNamesLabel;
         public HomeForm()
         {
 
@@ -33,9 +35,20 @@ namespace OOProjectBasedLeaning
                 Font = new Font("Arial", 16, FontStyle.Bold),
                 TextAlign = ContentAlignment.TopCenter
             };
+            //Labelの作成
+            employeeNamesLabel = new Label
+            {
+
+                Location = new Point(20, 20),
+                AutoSize = true,
+                Font = new Font("Arial", 12, FontStyle.Regular),
+
+            };
+
             //Formにコントロールを追加
             Controls.Add(ClockOutlabel);
             Controls.Add(timetrackerlabel);
+            Controls.Add(employeeNamesLabel);
             timetrackerlabel.BringToFront();
 
         }
@@ -55,9 +68,27 @@ namespace OOProjectBasedLeaning
                 (serializableObject as DragDropPanel).AddDragDropForm(this, PointToClient(new Point(dragEventArgs.X, dragEventArgs.Y)));
                 //操作したパネルをemployeePanelに定義
                 EmployeePanel employeePanel = (EmployeePanel)serializableObject;
+                //EmployyePanelからemPloyeeを取得
+                //Employee employee = employeePanel.returnEmp();
+                ////CompanyAddempの呼び出し
+                //CompanyAddEmp(employee);
                 employeePanel.AddForm(this);
             }
 
+        }
+        private void CompanyAddEmp(Employee employee)
+        {
+            //Employeeがcompanyに加入していないかの確認
+            Employee findEmp = company.FindEmployeeById(employee.Id);
+            //employeeがcompanyに加入していない時
+            if (findEmp != employee)
+            {
+                //companyにemployeeを加入
+                company.AddEmployee(employee);
+
+            }
+            //UpdateDisplayを呼び出し
+            //UpdateDisplay();
         }
 
     }
